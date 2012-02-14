b03cd8e75f8fbc33d73490fede0ffa24bdb589a6
Franco Catena <francocatena@gmail.com>
2012-02-14 13:50:15 -0300

    Improved responsive web design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,31 +4,34 @@ module ApplicationHelper
   end
   
   def pagination_links(objects, params = nil)
-    result = will_paginate objects, inner_window: 1, outer_window: 1,
-      params: params, renderer: BootstrapPaginationHelper::LinkRenderer,
+    result = will_paginate objects,
+      inner_window: 1, outer_window: 1, params: params,
+      renderer: BootstrapPaginationHelper::LinkRenderer,
       class: 'pagination pagination-right'
+    page_entries = content_tag(
+      :blockquote, content_tag(:small, page_entries_info(objects), class: 'page-entries pull-right')
+    )
     
     unless result
       previous_tag = content_tag(
         :li,
         content_tag(:a, t('will_paginate.previous_label').html_safe),
-        class: 'prev disabled'
+        class: 'previous_page disabled'
       )
       next_tag = content_tag(
         :li,
         content_tag(:a, t('will_paginate.next_label').html_safe),
         class: 'next disabled'
       )
-      current_tag = content_tag(:li, content_tag(:a, '1'), class: 'active')
       
       result = content_tag(
         :div,
-        content_tag(:ul, previous_tag + current_tag + next_tag),
+        content_tag(:ul, previous_tag + next_tag),
         class: 'pagination pagination-right'
       )
     end
 
-    result
+    result + page_entries
   end
   
   def link_to_edit(*args)
