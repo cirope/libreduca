896cd1436f53d7781e5630ab9a846ff43d47367c
Franco Catena <francocatena@gmail.com>
2012-03-21 10:12:55 -0300

    Pagination cleanup and improvement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,7 @@ class UsersController < ApplicationController
   def index
     @title = t 'view.users.index_title'
     @searchable = true
-    @users = @users.filtered_list(params[:q]).paginate(
-      page: params[:page], per_page: LINES_PER_PAGE
-    )
+    @users = @users.filtered_list(params[:q]).page(params[:page])
 
     respond_to do |format|
       format.html # index.html.erb
