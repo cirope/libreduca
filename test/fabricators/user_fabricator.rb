2ea992e39945d17749696bda613fa697dd15d7df
Franco Catena <francocatena@gmail.com>
2012-06-13 12:12:17 -0300

    Upgrade to Rails 3.2.6...

--- test/fabricators/user_fabricator.rb
+++ test/fabricators/user_fabricator.rb
@@ -1,8 +1,10 @@
 Fabricator(:user) do
   name { Faker::Name.first_name }
   lastname { Faker::Name.last_name }
-  email { |p| Faker::Internet.email([p.name, sequence(:user_id)].join(' ')) }
+  email { |attrs|
+    Faker::Internet.email([attrs[:name], sequence(:user_id)].join(' '))
+  }
   password { Faker::Lorem.sentence }
-  password_confirmation { |p| p.password }
+  password_confirmation { |attrs| attrs[:password] }
   role :admin
 end
