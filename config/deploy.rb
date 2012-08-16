e4fafe45d9d64dd29b72e342b5e100826eaa68e1
Franco Catena <francocatena@gmail.com>
2012-08-16 16:32:22 -0300

    Improve capistrano config...

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repository,  'https://github.com/francocatena/libreduca.git'
 set :deploy_to, '/var/rails/libreduca'
 set :user, 'deployer'
 set :group_writable, false
-set :shared_children, %w(log private public config)
+set :shared_children, %w(config log pids private public)
 set :use_sudo, false
 
 set :scm, :git
