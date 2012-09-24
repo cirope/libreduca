344c7b8247a67a82b41fd0f08aa34403a0515822
Franco Catena <francocatena@gmail.com>
2012-09-24 14:36:24 -0300

    Do not filter HTML in markdown...

--- config/initializers/redcarpet.rb
+++ config/initializers/redcarpet.rb
@@ -1,4 +1,4 @@
 MARKDOWN_RENDERER = Redcarpet::Markdown.new(
-  Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true),
+  Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: false),
   autolink: true, space_after_headers: true
 )
