5124e7634d5ef7a1e636917e784dfd5b2aeb544b
Franco Catena <francocatena@gmail.com>
2012-10-16 10:57:14 -0300

    Generic subdomain in email links when possible...

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -2,6 +2,7 @@ Libreduca::Application.configure do
   config.action_mailer.default_url_options = {
     host: APP_CONFIG['public_host'],
     protocol: (Rails.env.production? ? 'https' : 'http'),
+    subdomain: 'www'
   }
   
   config.action_mailer.smtp_settings = APP_CONFIG['smtp'].symbolize_keys
