5309774afd2ec308de7a4fb4eb630e7a6f886c49
Franco Catena <francocatena@gmail.com>
2013-02-04 20:34:52 -0300

    Added remove_avatar option to users...

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :lastname, :email, :password, :password_confirmation,
-    :avatar, :avatar_cache, :role, :remember_me, :kinships_attributes,
-    :jobs_attributes, :memberships_attributes,:lock_version
+    :avatar, :avatar_cache, :remove_avatar, :role, :remember_me,
+    :kinships_attributes, :jobs_attributes, :memberships_attributes,
+    :lock_version
 
   # Defaul order
   default_scope order("#{table_name}.lastname ASC")
