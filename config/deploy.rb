8df5f788f3c94a596aec6d4fc90a6379c78754af
Franco Catena <francocatena@gmail.com>
2013-03-06 12:54:51 -0300

    Add public/system in deploy linking

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repository,  'https://github.com/francocatena/libreduca.git'
 set :deploy_to, '/var/rails/libreduca'
 set :user, 'deployer'
 set :group_writable, false
-set :shared_children, %w(log private)
+set :shared_children, %w(log private public/system)
 set :use_sudo, false
 
 set :scm, :git
