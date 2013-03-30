f29a42da0134e22736ec5c28c40676d53d544c44
Franco Catena <francocatena@gmail.com>
2013-03-30 18:13:44 -0300

    Add question type selection in surveys...

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -6,4 +6,20 @@ module SurveysHelper
   def edit_survey_path(survey)
     polymorphic_path(survey_path(survey), action: :edit)
   end
+
+  def show_question_type_select(form)
+    options = Question::TYPES.map do |t|
+      [t("view.surveys.question_types.#{t}"), t]
+    end
+
+    form.input(
+      :question_type,
+      collection: options,
+      as: :select,
+      label: false,
+      input_html: {
+        class: 'span11', data: { 'templates-url' => 'holder' }
+      }
+    )
+  end
 end
