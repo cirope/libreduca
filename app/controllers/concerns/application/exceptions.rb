77ed34f776f705737ac4126dda95d207d65ac62f
Franco Catena <francocatena@gmail.com>
2013-04-24 15:03:04 -0300

    Application exception concern refector...

--- app/controllers/concerns/application/exceptions.rb
+++ app/controllers/concerns/application/exceptions.rb
@@ -7,19 +7,28 @@ module Application::Exceptions
         if exception.kind_of? CanCan::AccessDenied
           redirect_to root_url, alert: t('errors.access_denied')
         else
-          @title = t('errors.title')
-          
-          if response.redirect_url.blank?
-    render template: 'shared/show_error', locals: { error: exception }
-          end
-
-          logger.error(([exception, ''] + exception.backtrace).join("\n"))
+          render_error_page
+          log_exception(exception)
         end
 
       # In case the rescue explodes itself =)
       rescue => ex
-        logger.error(([ex, ''] + ex.backtrace).join("\n"))
+        log_exception(ex)
       end
     end
+  end
+
+  private
+
+  def render_error_page
+    @title = t('errors.title')
+    
+    if response.redirect_url.blank?
+      render template: 'shared/show_error', locals: { error: exception }
+    end
+  end
+
+  def log_exception(exception)
+    logger.error(([exception, ''] + exception.backtrace).join("\n"))
   end
 end
