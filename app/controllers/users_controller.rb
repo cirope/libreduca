44ae4479f3ad719c64566192e1bdb5659ecc81b9
Franco Catena <francocatena@gmail.com>
2013-04-24 14:55:13 -0300

    JSON responses removed from users controller...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
   load_and_authorize_resource
   
   # GET /users
-  # GET /users.json
   def index
     @title = t 'view.users.index_title'
     @searchable = true
@@ -14,30 +13,25 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @users }
       format.js   # index.js.erb
     end
   end
 
   # GET /users/1
-  # GET /users/1.json
   def show
     @title = t 'view.users.show_title'
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @user }
     end
   end
 
   # GET /users/new
-  # GET /users/new.json
   def new
     @title = t 'view.users.new_title'
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @user }
     end
   end
 
@@ -47,23 +41,19 @@ class UsersController < ApplicationController
   end
 
   # POST /users
-  # POST /users.json
   def create
     @title = t 'view.users.new_title'
 
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: t('view.users.correctly_created') }
-        format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PUT /users/1
-  # PUT /users/1.json
   def update
     authorize! :assign_roles, @user if params[:user] && params[:user][:roles]
     @title = t 'view.users.edit_title'
@@ -71,10 +61,8 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to @user, notice: t('view.users.correctly_updated') }
-        format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
     
@@ -89,17 +77,14 @@ class UsersController < ApplicationController
   end
   
   # PUT /users/1/update_profile
-  # PUT /users/1/update_profile.xml
   def update_profile
     @title = t('view.users.edit_profile')
     
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to(edit_profile_user_url(@user), notice: t('view.users.profile_correctly_updated')) }
-        format.xml  { head :ok }
       else
         format.html { render action: 'edit_profile' }
-        format.xml  { render xml: @user.errors, status: :unprocessable_entity }
       end
     end
 
@@ -109,13 +94,11 @@ class UsersController < ApplicationController
   end
 
   # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
 
     respond_to do |format|
       format.html { redirect_to users_url }
-      format.json { head :no_content }
     end
   end
   
