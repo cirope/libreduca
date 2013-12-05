90fdcc48878036fb20075967b11ed92fe16484b4
Ismael Isuani <ismaelisuani@gmail.com>
2013-12-05 09:22:39 -0300

    Fixed sent file for nginx

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Libreduca::Application.configure do
   config.assets.version = '1.0'
 
   # Specifies the header that your server uses for sending files.
-  config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  #config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for apache
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
