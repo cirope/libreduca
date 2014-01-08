87f47f2e4d71cf6167ff73697037c9d9acdfcb94
Franco Catena <francocatena@gmail.com>
2014-01-08 14:40:31 -0300

    Change the TLD config

--- config/initializers/tld.rb
+++ config/initializers/tld.rb
@@ -1,4 +1,1 @@
-Libreduca::Application.configure do
-  # Change default tld lenght
-  config.action_dispatch.tld_length = APP_CONFIG['public_host'].count('.')
-end
+ActionDispatch::Http::URL.tld_length = APP_CONFIG['public_host'].count('.')
