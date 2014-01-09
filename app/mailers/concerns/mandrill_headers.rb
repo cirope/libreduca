9184bb6c2104a740f5aa761155de18b740dc5cbf
Franco Catena <francocatena@gmail.com>
2014-01-09 19:09:19 -0300

    New way of kick devise emails =)

--- app/mailers/concerns/mandrill_headers.rb
+++ app/mailers/concerns/mandrill_headers.rb
@@ -1,13 +1,16 @@
 module MandrillHeaders
-  def initialize(method_name = nil, *args)
-    super.tap do
-      add_mandrillapp_headers if method_name
-    end
+  extend ActiveSupport::Concern
+
+  included do
+    after_action :add_mandrillapp_headers
   end
 
+  private
+
   def add_mandrillapp_headers
-    headers['X-MC-TrackingDomain'] = 'mail.libreduca.com'
-    headers['X-MC-SigningDomain'] = 'mail.libreduca.com'
-    headers['X-MC-Subaccount'] = 'libreduca'
+    headers['X-MC-ReturnPathDomain'] = 'mail.libreduca.com'
+    headers['X-MC-TrackingDomain']   = 'mail.libreduca.com'
+    headers['X-MC-SigningDomain']    = 'mail.libreduca.com'
+    headers['X-MC-Subaccount']       = 'libreduca'
   end
 end
