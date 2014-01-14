a0ddcd36d891e2ed71cff7ea9db0ff7e453a7dfe
Franco Catena <francocatena@gmail.com>
2014-01-14 19:46:19 -0300

    New environment config for Capistrano 3.1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,8 @@
 set :stage, :production
 set :rails_env, 'production'
 
-role :all, %w{libreduca.com}
+role :web, %w{deployer@libreduca.com}
+role :app, %w{deployer@libreduca.com}
+role :db,  %w{deployer@libreduca.com}
 
 server 'libreduca.com', user: 'deployer', roles: %w{web app db}
--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -1,6 +1,8 @@
 set :stage, :sandbox
 set :rails_env, 'sandbox'
 
-role :all, %w{localhost}
+role :web, %w{deployer@localhost}
+role :app, %w{deployer@localhost}
+role :db,  %w{deployer@localhost}
 
 server 'localhost', user: 'deployer', roles: %w{web app db}, ssh_options: { port: 2222 }
