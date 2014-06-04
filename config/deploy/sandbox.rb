0a7fdb671ef4e9563a1be90cd47f82c045716773
Franco Catena <francocatena@gmail.com>
2014-06-04 20:08:03 -0300

    Fix capistrano sandbox config

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -1,8 +1,9 @@
 set :stage, :sandbox
 set :rails_env, 'sandbox'
+set :ssh_options, port: 2222
 
 role :web, %w{deployer@localhost}
 role :app, %w{deployer@localhost}
 role :db,  %w{deployer@localhost}
 
-server 'localhost', user: 'deployer', roles: %w{web app db}, ssh_options: { port: 2222 }
+server 'localhost', user: 'deployer', roles: %w{web app db}
